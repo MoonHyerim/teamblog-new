c75e62514d45e715e4ddef67386fdc7c36c230d9
Chang-Ju Park <pcjpcj2@gmail.com>
2013-04-30 22:09:47 +0900

    board validation

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,9 @@
 class Board < ActiveRecord::Base
   attr_accessible :content, :count, :email, :name, :title
+
+  validates :name, presence: true, length: {minimum:3}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniquness: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
